bc5c38226326e5227496e0c117e6af3dca7b26fd
ogabek <norpulatovogabek42@gmail.com>
2022-06-30 22:53:38 +0500

    task

---
+++ bin/third.dart
@@ -0,0 +1,23 @@
+void main() {
+  print('------- WELCOME TO CANDY SHOP --------');
+  print('--- Pruducts being purchased by you: -');
+  double price1 = 40000;
+  double price2 = 65000;
+  double price3 = 30000;
+  double total = price2 + price3 + price1;
+
+  String x = '1. Alyonka';
+  String y = '2. Snickers';
+  String z = '3. Metro';
+  print("$x           ------    $price1");
+  print("$y          ------    $price2");
+  print("$z             ------    $price3");
+  //print('The total price is:');
+  double tax = 0;
+  if (total > 130000) {
+    tax = total - total * 5 / 100;
+  }
+  print('The total price is:  ======   $tax');
+  print("~~~ Thank you for your visiting!!! ~~~");
+  print("~~~~~~ Get up you here, please ~~~~~~~");
+}
